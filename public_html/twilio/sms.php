5cb03a48f35f7fdacd7ebfa0777afae0e7466f7a
James Warp <james@jnwarp.com>
2017-03-14 22:59:18 -0400

    Change HELP keyword to INFO

--- public_html/twilio/sms.php
+++ public_html/twilio/sms.php
@@ -33,24 +33,24 @@ if (preg_match($regex, $body, $email)) {
     $email = $email[0];
     $account->updateEmail($_POST['From'], $email);
 
-    $twilio->replySMS("Email updated to \"$email\", reply HELP for more information");
+    $twilio->replySMS("Email updated to \"$email\", reply INFO for more information");
 } else {
     switch(strtolower($body)) {
         case 'profile':
             $account->updateProfile($_POST['From'], $media_sid);
             $twilio->replySMS("Your profile picture has now been updated.");
             break;
-        case 'stop':
+        case 'deactivate':
             $twilio->replySMS("You will no longer receive any messages.");
             break;
-        case 'help':
+        case 'info':
             $email = $account->getEmail($_POST['From']);
-            
-            $twilio->replySMS("List of commands:\nHELP - Show this prompt\nSTOP - Deactivate account and stop recieving messages\nPROFILE - Set profile picture to next image recieved\n");
+
+            $twilio->replySMS("List of commands:\nINFO - Show this prompt\nDEACTIVATE - Deactivate account and stop recieving messages\nPROFILE - Set profile picture to next image recieved\n");
             $twilio->replySMS("Your email is currently set to \"$email\", reply with another email address to update it.");
             break;
         default:
-            $twilio->replySMS("Unknown command. Reply HELP for more info or STOP to stop receiving messages.");
+            $twilio->replySMS("Unknown command, reply INFO for more info.");
     }
 }
 
