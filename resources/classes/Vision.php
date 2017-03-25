4bc83404ab456e22e5bb09ec7ef95cdc5e0e4966
James Warp <james@jnwarp.com>
2017-03-25 14:25:23 -0400

    Update request body

--- resources/classes/Vision.php
+++ resources/classes/Vision.php
@@ -12,31 +12,39 @@ class Vision
         global $config;
 
         // create the request body
-        $request = json_encode([
-            'requests' => [
-                'image' => $image_data,
-                'features' => [
-                    'type' => 'DOCUMENT_TEXT_DETECTION'
-                ]
-            ]
-        ]);
+        $request = <<<EOT
+{
+ "requests": [
+  {
+   "features": [
+    {
+     "type": "DOCUMENT_TEXT_DETECTION"
+    }
+   ],
+   "image": {
+    "content": "$image_data"
+   }
+  }
+ ]
+}
+EOT;
 
         // create the url request
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL,
+    		$curl = curl_init();
+    		curl_setopt($curl, CURLOPT_URL,
             'https://vision.googleapis.com/v1/images:annotate?key=' .
             $config['vision']['api_key']
         );
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_HTTPHEADER,
-			["Content-type: application/json")];
-		curl_setopt($curl, CURLOPT_POST, true);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER,
+        ["Content-type: application/json"]);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
 
         // send the request
-		$result = curl_exec($curl);
-		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		curl_close($curl);
+        $result = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
 
         return $result;
     }
